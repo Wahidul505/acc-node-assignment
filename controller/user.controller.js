9d4f16fb649c50ca94c5877407e6ed16ad20f688
MacBook <macbookair@MacBooks-MacBook-Air.local>
2022-09-03 11:40:37 +0600

    created method + endpoint to get random user

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,10 +1,34 @@
 const fs = require('fs');
+const getRandomIndex = require('../utils/getRandomIndex');
 
 module.exports.getAllUsers = (req, res) => {
     fs.readFile('userData.json', (error, data) => {
         error ?
-            res.send({ success: 'false' })
+            res.send({ success: false })
             :
-            res.send({ success: true, data: JSON.parse(data) });
+            res.send({ success: true, data: JSON.parse(data) })
+    })
+};
+
+module.exports.getARandomUser = (req, res) => {
+    fs.readFile('userData.json', (error, data) => {
+
+        if (error) {
+            res.send({ success: false })
+        }
+        else {
+            const getRandomNumber = () => {
+                const randomNumber = Math.floor(Math.random() * 100);
+                if (randomNumber < JSON.parse(data).length) {
+                    return randomNumber;
+                } else {
+                    return getRandomNumber();
+                }
+            };
+
+            const randomIndex = getRandomNumber();
+            const users = JSON.parse(data);
+            res.send({ success: true, data: users[randomIndex] });
+        }
     })
 }
--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { getAllUsers } = require('../../controller/user.controller');
+const { getAllUsers, getARandomUser } = require('../../controller/user.controller');
 const router = express.Router();
 
 router.get('/all', getAllUsers);
+router.get('/random', getARandomUser);
 
 module.exports = router;
