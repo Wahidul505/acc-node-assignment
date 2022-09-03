fdae6844678271cfc4fac138ac02f9de0cfaa8a2
MacBook <macbookair@MacBooks-MacBook-Air.local>
2022-09-03 13:59:08 +0600

    implemented method for bulk update

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,98 +4,163 @@ const getRandomIndex = require('../utils/getRandomIndex');
 
 // to get all the users from userData.json 
 module.exports.getAllUsers = (req, res) => {
-    fs.readFile('userData.json', (error, data) => {
-        error ?
-            res.send({ success: false })
-            :
-            res.send({ success: true, data: JSON.parse(data) })
-    })
+    try {
+        fs.readFile('userData.json', (error, data) => {
+            error ?
+                res.send({ success: false })
+                :
+                res.send({ success: true, data: JSON.parse(data) })
+        })
+    }
+    catch {
+        res.send({ success: false });
+    }
 };
 
 // to get a random user from userData.json 
 module.exports.getARandomUser = (req, res) => {
-    fs.readFile('userData.json', (error, data) => {
-
-        if (error) {
-            res.send({ success: false })
-        }
-        else {
-            const getRandomNumber = () => {
-                const randomNumber = Math.floor(Math.random() * 100);
-                if (randomNumber < JSON.parse(data).length) {
-                    return randomNumber;
-                } else {
-                    return getRandomNumber();
-                }
-            };
-
-            const randomIndex = getRandomNumber();
-            const users = JSON.parse(data);
-            res.send({ success: true, data: users[randomIndex] });
-        }
-    })
-};
-
-// to save a new user in userData.json 
-module.exports.saveAUser = (req, res) => {
-    const newUser = req.body;
-    if ((newUser.id && newUser.gender && newUser.name && newUser.contact && newUser.address && newUser.photoUrl)) {
+    try {
         fs.readFile('userData.json', (error, data) => {
+
             if (error) {
                 res.send({ success: false })
             }
             else {
+                const getRandomNumber = () => {
+                    const randomNumber = Math.floor(Math.random() * 100);
+                    if (randomNumber < JSON.parse(data).length) {
+                        return randomNumber;
+                    } else {
+                        return getRandomNumber();
+                    }
+                };
+
+                const randomIndex = getRandomNumber();
                 const users = JSON.parse(data);
-                users.push(newUser);
-                fs.writeFile('userData.json', JSON.stringify(users), (error) => {
-                    error ?
-                        res.send({ success: false })
-                        :
-                        res.send({ success: true, message: "User Successfully created!" })
-                })
+                res.send({ success: true, data: users[randomIndex] });
             }
         })
     }
-    else {
-        res.send({ success: false, message: "Please provide all required properties to create a user" });
+    catch {
+        res.send({ success: false });
+    }
+};
+
+// to save a new user in userData.json 
+module.exports.saveAUser = (req, res) => {
+    try {
+        const newUser = req.body;
+        if ((newUser.id && newUser.gender && newUser.name && newUser.contact && newUser.address && newUser.photoUrl)) {
+            fs.readFile('userData.json', (error, data) => {
+                if (error) {
+                    res.send({ success: false })
+                }
+                else {
+                    const users = JSON.parse(data);
+                    users.push(newUser);
+                    fs.writeFile('userData.json', JSON.stringify(users), (error) => {
+                        error ?
+                            res.send({ success: false })
+                            :
+                            res.send({ success: true, message: "User Successfully created!" })
+                    })
+                }
+            })
+        }
+        else {
+            res.send({ success: false, message: "Please provide all required properties to create a user" });
+        }
+    }
+    catch {
+        res.send({ success: false });
     }
 };
 
 // to update a particular user based on id
 module.exports.updateAUser = (req, res) => {
-    const { id, ...rest } = req.body;
-    fs.readFile("userData.json", (error, data) => {
-        if (error) {
-            res.send({ success: false })
-        }
-        else {
-            const users = JSON.parse(data);
-            const existUser = users.find(user => user.id === Number(id));
-            if (existUser) {
-                const restUsers = users.filter(user => user.id !== Number(id));
-                const updateEntries = Object.entries(rest);
-                for (const key in existUser) {
-                    updateEntries.forEach(entries => {
-                        if (key === entries[0]) {
-                            existUser[key] = entries[1];
-                        } else {
-                            return;
+    try {
+        const { id, ...rest } = req.body;
+        fs.readFile("userData.json", (error, data) => {
+            if (error) {
+                res.send({ success: false })
+            }
+            else {
+                const users = JSON.parse(data);
+                const existUser = users.find(user => user.id === Number(id));
+                if (existUser) {
+                    const restUsers = users.filter(user => user.id !== Number(id));
+                    const updateEntries = Object.entries(rest);
+                    for (const key in existUser) {
+                        updateEntries.forEach(entries => {
+                            if (key === entries[0]) {
+                                existUser[key] = entries[1];
+                            } else {
+                                return;
+                            }
+                        })
+                    }
+                    restUsers.push(existUser);
+                    fs.writeFile('userData.json', JSON.stringify(restUsers), (error) => {
+                        error ?
+                            res.send({ success: false, message: "Fail to Update..." })
+                            :
+                            res.send({ success: true, message: "User Updated Successfully!" });
+                    })
+                }
+                else {
+                    res.send({ success: false, message: "User Not Found!" })
+                }
+            }
+        })
+    }
+    catch {
+        res.send({ success: false });
+    }
+};
+
+
+// to bulk update multiple users 
+module.exports.updateMultipleUsers = (req, res) => {
+    try {
+        const updatingUsers = req.body;
+
+        fs.readFile("userData.json", (error, data) => {
+            if (error) {
+                res.send({ success: false })
+            }
+            else {
+                const users = JSON.parse(data);
+                let restUsers;
+                for (const updatingUser of updatingUsers) {
+                    // const exist = users.find(user => user.id === updatingUser.id);
+                    restUsers = users.filter(user => user.id !== updatingUser.id);
+                    users.forEach(user => {
+                        if (user.id === updatingUser.id) {
+                            const updateEntries = Object.entries(updatingUser);
+                            for (const key in user) {
+                                updateEntries.forEach(entries => {
+                                    if (key === entries[0]) {
+                                        user[key] = entries[1];
+                                    } else {
+                                        return;
+                                    }
+                                })
+                            }
+                            restUsers.push(user);
                         }
                     })
                 }
-                restUsers.push(existUser);
-                fs.writeFile('userData.json', JSON.stringify(restUsers), (error) => {
+                fs.writeFile("userData.json", JSON.stringify(restUsers), (error) => {
                     error ?
-                        res.send({ success: false, message: "Fail to Update..." })
+                        res.send({ success: false, message: "Fail to Update Users..." })
                         :
-                        res.send({ success: true, message: "User Updated Successfully!" });
+                        res.send({ success: true, message: "Users Updated Successfully!" });
                 })
             }
-            else {
-                res.send({ success: false, message: "User Not Found!" })
-            }
-        }
-    })
+        })
+    }
+    catch {
+        res.send({ success: false });
+    }
+};
 
-
-}
--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getAllUsers, getARandomUser, saveAUser, updateAUser } = require('../../controller/user.controller');
+const { getAllUsers, getARandomUser, saveAUser, updateAUser, updateMultipleUsers } = require('../../controller/user.controller');
 const router = express.Router();
 
 router.get('/all', getAllUsers);
 router.get('/random', getARandomUser);
 router.post('/save', saveAUser);
-router.patch('/update', updateAUser)
+router.patch('/update', updateAUser);
+router.patch('/bulk-update', updateMultipleUsers);
 
 module.exports = router;
